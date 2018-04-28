1b2be47d69296ae9debb0c82b96b0e972e78d64d
frederiek de wolf <frederiek.dewolf@avondschool.hogent.be>
2018-04-28 15:59:39 +0200

    Initial commit

---
+++ app/src/main/java/com/example/activitylifecyclecallbackmethodsapp/FirstActivity.java
@@ -0,0 +1,61 @@
+package com.example.activitylifecyclecallbackmethodsapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class FirstActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first);
+
+        Log.i(TAG, "++++++++++ onCreate(" + (savedInstanceState != null ? "savedInstanceState" : "null") + ")");
+    }
+
+    private static final String TAG = "TRACE FirstActivity";
+    @Override protected void onStart() {
+        Log.i(TAG, "onStart()");
+        super.onStart();
+    }
+    @Override protected void onResume() {
+        Log.i(TAG, "onResume()");
+        super.onResume();
+    }
+    @Override protected void onPause() {
+        Log.i(TAG, "onPause()");
+        super.onPause();
+    }
+    @Override protected void onStop() {
+        Log.i(TAG, "onStop()");
+        super.onStop();
+    }
+    @Override protected void onRestart() {
+        Log.i(TAG, "onRestart()");
+        super.onRestart();
+    }
+    @Override protected void onDestroy() {
+        Log.i(TAG, "---------- onDestroy()");
+        super.onDestroy();
+    }
+    @Override protected void onSaveInstanceState(Bundle outState) {
+        Log.i(TAG, "onSaveInstanceState(outState)");
+        super.onSaveInstanceState(outState);
+    }
+    @Override protected void onRestoreInstanceState(Bundle savedState) {
+        Log.i(TAG, "onRestoreInstanceState(savedInstanceState)");
+        super.onRestoreInstanceState(savedState);
+    }
+
+    public void gotoSecondActivity(View v){
+        Log.d(TAG, "gotoSecondActivity() ..........");
+        startActivity(new Intent(this, SecondActivity.class));
+    }
+    public void finishThisActivity(View v){
+        Log.d(TAG, "finish() ..........");
+        finish();
+    }
+}
---
+++ app/src/main/java/com/example/activitylifecyclecallbackmethodsapp/SecondActivity.java
@@ -0,0 +1,64 @@
+package com.example.activitylifecyclecallbackmethodsapp;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        Log.i(TAG, "++++++++++ onCreate(" + (savedInstanceState != null ? "savedInstanceState" : "null") + ")");
+    }
+
+    private static final String TAG = "TRACE SecondActivity";
+    @Override protected void onStart() {
+        Log.i(TAG, "onStart()");
+        super.onStart();
+    }
+    @Override protected void onResume() {
+        Log.i(TAG, "onResume()");
+        super.onResume();
+    }
+    @Override protected void onPause() {
+        Log.i(TAG, "onPause()");
+        super.onPause();
+    }
+    @Override protected void onStop() {
+        Log.i(TAG, "onStop()");
+        super.onStop();
+    }
+    @Override protected void onRestart() {
+        Log.i(TAG, "onRestart()");
+        super.onRestart();
+    }
+    @Override protected void onDestroy() {
+        Log.i(TAG, "---------- onDestroy()");
+        super.onDestroy();
+    }
+    @Override protected void onSaveInstanceState(Bundle outState) {
+        Log.i(TAG, "onSaveInstanceState(outState)");
+        super.onSaveInstanceState(outState);
+    }
+    @Override protected void onRestoreInstanceState(Bundle savedState) {
+        Log.i(TAG, "onRestoreInstanceState(savedInstanceState)");
+        super.onRestoreInstanceState(savedState);
+    }
+
+    public void finishThisActivity(View v){
+        Log.d(TAG, "finish() ..........");
+        finish();
+    }
+
+}
